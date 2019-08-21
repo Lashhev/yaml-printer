ea53035174b343f713cd9bfb00157af35e19f589
Andrew Lashchev <lashhev-andrej@yandex.ru>
2019-08-21 22:22:05 +0400

    converting of ros messages to yaml node was disabled

--- include/yaml-node.hpp
+++ include/yaml-node.hpp
@@ -6,7 +6,9 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sstream"
 #include <memory>
-#include <yaml-cpp/yaml.h>
+//#include <yaml-cpp/yaml.h>
+
+using std::placeholders::_1;
 class YamlNodeBase
 {
     public:
@@ -24,13 +26,28 @@ template<class MessageT>
 class YamlNode: public YamlNodeBase
 {
     public:
-        YamlNode(int argc, char * argv[])
-        {
-            
-        }
-
+        YamlNode();
     private:
     void topic_callback_(const std::shared_ptr<MessageT> msg);
 };
+template <class MessageT>
+void YamlNode<MessageT>::topic_callback_(const std::shared_ptr<MessageT> msg)
+{
+ // YAML::Node ros_msg;
+  std::stringstream output;
+
+  RCLCPP_INFO(yaml_node_->get_logger(),"\nGot it!");
+}
+template <class MessageT>
+YamlNode<MessageT>::YamlNode()
+{
+  yaml_node_ = rclcpp::Node::make_shared("yaml_printer");
+  subscription_ = yaml_node_->create_subscription<MessageT>
+      ("topic", std::bind(&YamlNode<MessageT>::topic_callback_, this, _1));
+  rclcpp::spin(yaml_node_);
+  rclcpp::shutdown();
+  subscription_ = nullptr;
+  yaml_node_ = nullptr;
+}
 
 #endif
