0ba0fe0065c14eb3299daf0d15b62eaf5e3872e0
Andrew Lashchev <lashhev-andrej@yandex.ru>
2019-08-21 22:25:27 +0400

    parsing arguments was added

--- src/node.cpp
+++ src/node.cpp
@@ -1,8 +1,12 @@
 #include "building_interface.hpp"
-
+//#include "yaml-helper.cpp"
 int main(int argc, char * argv[])
 {
-  std::string msg_name = "sensor_msgs/Joy";
+  if(argc < 2 && argc > 2)
+    return 0;
+  rclcpp::init(argc, argv);
+  std::string msg_name = argv[1];
+  printf("%s\n",msg_name.c_str() );
   std::shared_ptr<YamlNodeBase> topic_node = get_yaml_node(msg_name);
   return 0;
 }
